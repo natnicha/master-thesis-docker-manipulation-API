629c6a62928a4587e9f3e120cc8012efc56c98b0
Natnicha <nat.rodtong@gmail.com>
2024-09-24 15:04:32 +0200

    feat: add scale-in and scale-out routes

--- server/app.py
+++ server/app.py
@@ -8,10 +8,17 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
   
-@app.route('/containers')
+@app.route('/containers', methods=['GET'])
 def containers():   
     containers_info = get_containers_info()
     return {
         'containers': containers_info
     }
 
+@app.route('/scale/in', methods=['POST'])
+def scale_in():
+    return {}
+
+@app.route('/scale/out', methods=['POST'])
+def scale_in():
+    return {}
