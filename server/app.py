c68f78d0cb9ea3c219272a59b345ed712453a57d
Natnicha <nat.rodtong@gmail.com>
2024-09-24 17:00:46 +0200

    feat: add scaling pod in & out functions

--- server/app.py
+++ server/app.py
@@ -1,24 +1,25 @@
 from flask import Flask
 from model import *
 from containers import *
+from pods import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+  return 'Hello, World!'
   
 @app.route('/containers', methods=['GET'])
 def containers():   
-    containers_info = get_containers_info()
-    return {
-        'containers': containers_info
-    }
+  containers_info = get_containers_info()
+  return {
+    'containers': containers_info
+  }
 
 @app.route('/scale/in', methods=['POST'])
 def scale_in():
-    return {}
+  return scale_service_in()
 
 @app.route('/scale/out', methods=['POST'])
 def scale_out():
-    return {}
+  return scale_service_out()
