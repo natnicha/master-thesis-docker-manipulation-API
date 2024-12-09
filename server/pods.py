648fc2fa1fde893b15bfd33685e70f801cb19a36
Natnicha <nat.rodtong@gmail.com>
2024-12-09 18:07:53 +0100

    fix: change pods service, use kubernetes client

--- server/pods.py
+++ server/pods.py
@@ -1,21 +1,28 @@
-import docker
 import logging
 from datetime import datetime
-
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+import containers
 MIN_REPLICAS = 1
-MAX_REPLICAS = 5
+MAX_REPLICAS = 10
 
 class ScalingPod():
   In = -1
   Out = 1
 
-def get_service_info():
-  client = docker.from_env()
-  services = client.services.list(filters={"name":["ml_app"]})
-  return services[0]
+def scale_pods(replicas: int):
+  try:
+    configuration = config.load_kube_config()
+
+    api_client = client.ApiClient(configuration)
+    apps_v1 = client.AppsV1Api(api_client)
+    api_response = apps_v1.patch_namespaced_deployment_scale('ml-app', "default", {"spec": {"replicas": replicas}})
+    print(api_response)
+  except ApiException as e:
+    print("Exception when calling AppsV1Api->patch_namespaced_deployment_scale: %s\n" % e)
 
 def get_service_replicas(service_info: dict):
-  return service_info.attrs['Spec']['Mode']['Replicated']['Replicas']
+  return service_info['system']['online_pods']
 
 def scale_service_in():
   return scale_service(ScalingPod.In)
@@ -24,12 +31,12 @@ def scale_service_out():
   return scale_service(ScalingPod.Out)
 
 def scale_service(scale: ScalingPod):
-  target_service = get_service_info()
+  target_service = containers.get_containers_info()
   current_replicas = get_service_replicas(target_service)
   target_replicas = current_replicas + scale
   if target_replicas >= MIN_REPLICAS and target_replicas <= MAX_REPLICAS:
     start_dateTime = datetime.now()
-    target_service.scale(target_replicas)
+    scale_pods(target_replicas)
     check_container_status(target_replicas)
     finish_dateTime = datetime.now()
     time_spent = finish_dateTime-start_dateTime
@@ -50,18 +57,14 @@ def scale_service(scale: ScalingPod):
 
 
 def check_container_status(target_replicas: int):
-  client = docker.from_env()
-  timeout = 30
+  timeout = 90
   is_all_pod_running = False
   start_time = datetime.now()
+  api = client.CustomObjectsApi()
   while(not is_all_pod_running and ((datetime.now()-start_time).total_seconds() < timeout)):
-    containers = client.containers.list(filters={"ancestor": "master-thesis-image-recognition-app"})
-    if target_replicas != len(containers):
+    k8s_pods = api.list_namespaced_custom_object("metrics.k8s.io", "v1beta1", "default", "pods")
+    if target_replicas != len(k8s_pods['items']):
       continue
-    for container in containers:
-      if container.status != 'running':
-        is_all_pod_running = False
-        break
-      if container.status == 'running':
-        is_all_pod_running = True
+    else:
+      is_all_pod_running = True
   return 
